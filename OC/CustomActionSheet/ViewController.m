f531fe53b24fcf7e7cf96c4587d3b872fbe4193c
Smartech <rain2540@foxmail.com>
2015-12-22 10:50:49 +0800

    Add an explain

--- OC/CustomActionSheet/ViewController.m
+++ OC/CustomActionSheet/ViewController.m
@@ -21,6 +21,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark View Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
